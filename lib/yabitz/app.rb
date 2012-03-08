29ade82a31d51c3990d58a7f3975fd7fb25fa6d1
ytnobody / satoshi azuma <ytnobody@gmail.com>
2012-03-08 16:38:37 +0900

    move each "/ybz/charge/*" handlers into controller/charge.rb

--- lib/yabitz/app.rb
+++ lib/yabitz/app.rb
@@ -165,35 +165,6 @@ class Yabitz::Application < Sinatra::Base
   # get '/ybz/dnsname/floating' #TODO
   # delete '/ybz/dnsname/:oid' #TODO
 
-  ### 課金状況
-  # 全体
-  get %r!/ybz/charge/summary(\.tsv)?! do |ctype|
-    authorized?
-    @depts = Yabitz::Model::Dept.all
-    @contents = Yabitz::Model::Content.all
-    @services = Yabitz::Model::Service.all
-    @hosts = Yabitz::Model::Host.all
-    tmp3 = Yabitz::Model::HwInformation.all
-
-    @status, @types, @chargings, @dept_counts, @content_counts = Yabitz::Charging.calculate(@hosts)
-
-    case ctype
-    when '.tsv' then raise NotImplementedError
-    else
-      @hide_selectionbox = true
-      @page_title = "課金用情報サマリ"
-      haml :charge_summary
-    end
-  end
-  # コンテンツごと
-  get %r!/ybz/charge/content/(\d+)\.ajax! do |oid|
-    authorized?
-    
-    @content = Yabitz::Model::Content.get(oid.to_i)
-    @content_charges = Yabitz::Charging.calculate_content(@content)
-    haml :charge_content_parts, :layout => false
-  end
-
   ### ユーザ情報 (すべて認証要求、post/putはadmin)
   get '/ybz/auth_info/list' do
     protected!
--- lib/yabitz/controller/charge.rb
+++ lib/yabitz/controller/charge.rb
@@ -5,5 +5,33 @@ require 'sinatra/base'
 require 'haml'
 
 class Yabitz::Application < Sinatra::Base
-  # handlers
+  ### 課金状況
+  # 全体
+  get %r!/ybz/charge/summary(\.tsv)?! do |ctype|
+    authorized?
+    @depts = Yabitz::Model::Dept.all
+    @contents = Yabitz::Model::Content.all
+    @services = Yabitz::Model::Service.all
+    @hosts = Yabitz::Model::Host.all
+    tmp3 = Yabitz::Model::HwInformation.all
+
+    @status, @types, @chargings, @dept_counts, @content_counts = Yabitz::Charging.calculate(@hosts)
+
+    case ctype
+    when '.tsv' then raise NotImplementedError
+    else
+      @hide_selectionbox = true
+      @page_title = "課金用情報サマリ"
+      haml :charge_summary
+    end
+  end
+  # コンテンツごと
+  get %r!/ybz/charge/content/(\d+)\.ajax! do |oid|
+    authorized?
+    
+    @content = Yabitz::Model::Content.get(oid.to_i)
+    @content_charges = Yabitz::Charging.calculate_content(@content)
+    haml :charge_content_parts, :layout => false
+  end
+
 end
