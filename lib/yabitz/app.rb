d842a8521a2c6fae772c454d6d58597d13104f50
ytnobody / satoshi azuma <ytnobody@gmail.com>
2012-03-08 16:43:26 +0900

    move each "/ybz/auth_info/*" handlers into controller/auth_info.rb

--- lib/yabitz/app.rb
+++ lib/yabitz/app.rb
@@ -165,60 +165,6 @@ class Yabitz::Application < Sinatra::Base
   # get '/ybz/dnsname/floating' #TODO
   # delete '/ybz/dnsname/:oid' #TODO
 
-  ### ユーザ情報 (すべて認証要求、post/putはadmin)
-  get '/ybz/auth_info/list' do
-    protected!
-    all_users = Yabitz::Model::AuthInfo.all.sort
-    valids = []
-    invalids = []
-    all_users.each do |u|
-      if u.valid? and not u.root?
-        valids.push(u)
-      elsif not u.root?
-        invalids.push(u)
-      end
-    end
-    @users = valids + invalids
-    @page_title = "ユーザ認証情報一覧"
-    haml :auth_info_list
-  end
-
-  get %r!/ybz/auth_info/(\d+)(\.ajax|\.tr\.ajax)?! do |oid, ctype|
-    protected!
-    @auth_info = Yabitz::Model::AuthInfo.get(oid.to_i)
-    pass unless @auth_info # object not found -> HTTP 404
-
-    case ctype
-    when '.ajax' then haml :auth_info_parts, :layout => false
-    when '.tr.ajax' then haml :auth_info, :layout => false, :locals => {:auth_info => @auth_info}
-    else
-      raise NotImplementedError
-    end
-  end
-  
-  post '/ybz/auth_info/:oid' do
-    admin_protected!
-    user = Yabitz::Model::AuthInfo.get(params[:oid].to_i)
-    pass unless user
-
-    case request.params['operation']
-    when 'toggle'
-      case request.params['field']
-      when 'priv'
-        if user.admin?
-          user.priv = nil
-        else
-          user.set_admin
-        end
-      when 'valid'
-        user.valid = (not user.valid?)
-      end
-    end
-    user.save
-    "ok"
-  end
-  # post '/ybz/auth_info/invalidate' #TODO
-
   get '/ybz/yabitz.css' do
     authorized?
     content_type 'text/css', :charset => 'utf-8'
--- lib/yabitz/controller/auth_info.rb
+++ lib/yabitz/controller/auth_info.rb
@@ -5,5 +5,58 @@ require 'sinatra/base'
 require 'haml'
 
 class Yabitz::Application < Sinatra::Base
-  # handlers
+  ### ユーザ情報 (すべて認証要求、post/putはadmin)
+  get '/ybz/auth_info/list' do
+    protected!
+    all_users = Yabitz::Model::AuthInfo.all.sort
+    valids = []
+    invalids = []
+    all_users.each do |u|
+      if u.valid? and not u.root?
+        valids.push(u)
+      elsif not u.root?
+        invalids.push(u)
+      end
+    end
+    @users = valids + invalids
+    @page_title = "ユーザ認証情報一覧"
+    haml :auth_info_list
+  end
+
+  get %r!/ybz/auth_info/(\d+)(\.ajax|\.tr\.ajax)?! do |oid, ctype|
+    protected!
+    @auth_info = Yabitz::Model::AuthInfo.get(oid.to_i)
+    pass unless @auth_info # object not found -> HTTP 404
+
+    case ctype
+    when '.ajax' then haml :auth_info_parts, :layout => false
+    when '.tr.ajax' then haml :auth_info, :layout => false, :locals => {:auth_info => @auth_info}
+    else
+      raise NotImplementedError
+    end
+  end
+  
+  post '/ybz/auth_info/:oid' do
+    admin_protected!
+    user = Yabitz::Model::AuthInfo.get(params[:oid].to_i)
+    pass unless user
+
+    case request.params['operation']
+    when 'toggle'
+      case request.params['field']
+      when 'priv'
+        if user.admin?
+          user.priv = nil
+        else
+          user.set_admin
+        end
+      when 'valid'
+        user.valid = (not user.valid?)
+      end
+    end
+    user.save
+    "ok"
+  end
+  # post '/ybz/auth_info/invalidate' #TODO
+
 end
