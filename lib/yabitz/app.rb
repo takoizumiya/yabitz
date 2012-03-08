1d13c56986e7a8e1436f1479a3134508a7bf691c
ytnobody / satoshi azuma <ytnobody@gmail.com>
2012-03-08 16:53:10 +0900

    move "/ybz/smartsearch" handler into controller/smartsearch.rb

--- lib/yabitz/app.rb
+++ lib/yabitz/app.rb
@@ -72,45 +72,6 @@ class Yabitz::Application < Sinatra::Base
     request.body.read # and throw away to somewhere...
   end
 
-  ### smart search ###
-  get %r!/ybz/smartsearch(\.json|\.csv)?! do |ctype|
-    authorized?
-    searchparams = request.params['keywords'].strip.split(/\s+/)
-    @page_title = "簡易検索 結果"
-    @service_results = []
-    @results = []
-    @brick_results = []
-    searchparams.each do |keyword|
-      search_props = Yabitz::SmartSearch.kind(keyword)
-      search_props.each do |type, name, model|
-        if model == :service
-          @service_results.push([name + ": " + keyword, Yabitz::SmartSearch.search(type, keyword)])
-        elsif model == :brick
-          @brick_results.push([name + ": " + keyword, Yabitz::SmartSearch.search(type, keyword)])
-        else
-          @results.push([name + ": " + keyword, Yabitz::SmartSearch.search(type, keyword)])
-        end
-      end
-    end
-
-    Stratum.preload(@results.map(&:last).flatten, Yabitz::Model::Host) if @results.size > 0 and @results.map(&:last).flatten.size > 0
-    case ctype
-    when '.json'
-      response['Content-Type'] = 'application/json'
-      # ignore service/brick list for json
-      @results.map(&:last).flatten.to_json
-    when '.csv'
-      response['Content-Type'] = 'text/csv'
-      # ignore service/brick list for csv
-      Yabitz::Model::Host.build_raw_csv(Yabitz::Model::Host::CSVFIELDS_LL, @results.map(&:last).flatten)
-    else
-      @copypastable = true
-      @service_unselectable = true
-      @brick_unselectable = true
-      haml :smartsearch, :locals => {:cond => searchparams.join(' ')}
-    end
-  end
-
   ### 管理用 情報閲覧・操作 ###
   
   # hostに対して service,contact,dnsname,ipaddress,rackunit,hwidの欠落および重複をチェックして一覧出力
