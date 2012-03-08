a37ce892f5f6f740686206c36944779d752a8eed
ytnobody / satoshi azuma <ytnobody@gmail.com>
2012-03-08 16:47:04 +0900

    move each "/ybz/authentication/*" handlers into controller/authentication.rb

--- lib/yabitz/app.rb
+++ lib/yabitz/app.rb
@@ -72,35 +72,6 @@ class Yabitz::Application < Sinatra::Base
     request.body.read # and throw away to somewhere...
   end
 
-  ### 認証 ###
-  get '/ybz/authenticate/login' do
-    protected!
-    if request.referer.nil? or request.referer.empty? or request.referer == '/'
-      redirect '/ybz/services'
-    end
-    redirect request.referer
-  end
-
-  get '/ybz/authenticate/logout' do
-    session[:username] = ""
-    redirect '/ybz/services'
-  end
-
-  get '/ybz/authenticate/basic' do
-    protected!
-    "ok"
-  end
-
-  post '/ybz/authenticate/form' do
-    pair = request.params().values_at(:username, :password)
-    user = Yabitz::Model::AuthInfo.authenticate(*pair, request.ip)
-    unless user
-      response['WWW-Authenticate'] = %(Basic realm=BASIC_AUTH_REALM)
-      throw(:halt, [401, "Not Authorized\n"])
-    end
-    "ok"
-  end
-
   ### smart search ###
   get %r!/ybz/smartsearch(\.json|\.csv)?! do |ctype|
     authorized?
--- lib/yabitz/controller/authenticate.rb
+++ lib/yabitz/controller/authenticate.rb
@@ -5,5 +5,33 @@ require 'sinatra/base'
 require 'haml'
 
 class Yabitz::Application < Sinatra::Base
-  # handlers
+  ### 認証 ###
+  get '/ybz/authenticate/login' do
+    protected!
+    if request.referer.nil? or request.referer.empty? or request.referer == '/'
+      redirect '/ybz/services'
+    end
+    redirect request.referer
+  end
+
+  get '/ybz/authenticate/logout' do
+    session[:username] = ""
+    redirect '/ybz/services'
+  end
+
+  get '/ybz/authenticate/basic' do
+    protected!
+    "ok"
+  end
+
+  post '/ybz/authenticate/form' do
+    pair = request.params().values_at(:username, :password)
+    user = Yabitz::Model::AuthInfo.authenticate(*pair, request.ip)
+    unless user
+      response['WWW-Authenticate'] = %(Basic realm=BASIC_AUTH_REALM)
+      throw(:halt, [401, "Not Authorized\n"])
+    end
+    "ok"
+  end
+
 end
